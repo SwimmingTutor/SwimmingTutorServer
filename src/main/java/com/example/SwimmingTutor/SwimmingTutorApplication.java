9077ae27d06a655988d253be474127e02e7ac2f9
ekmbjh <ekmbjh@naver.com>
2024-06-08 14:02:57 +0900

    Initial commit

---
+++ src/main/java/com/example/SwimmingTutor/SwimmingTutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwimmingTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingTutorApplication.class, args);
+	}
+
+}
