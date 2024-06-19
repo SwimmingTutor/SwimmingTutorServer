f27f7ee9cc5157191fa7e03336ca3ea61c10b82a
danmiee <dan-mi@kakao.com>
2024-06-20 04:33:21 +0900

    Rename: 해당 브랜치에서 불필요한 폴더 삭제

--- src/main/java/kr/sesacjava/swimtutor/leveltest/repository/LevelClassificationRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.sesacjava.swimtutor.leveltest.repository;
-
-import kr.sesacjava.swimtutor.leveltest.entity.LevelClassification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LevelClassificationRepository extends JpaRepository<LevelClassification, Long> {
-}
--- src/main/java/kr/sesacjava/swimtutor/leveltest/repository/LevelLogRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.sesacjava.swimtutor.leveltest.repository;
-
-import kr.sesacjava.swimtutor.leveltest.entity.LevelLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LevelLogRepository extends JpaRepository<LevelLog, Long> {
-}
