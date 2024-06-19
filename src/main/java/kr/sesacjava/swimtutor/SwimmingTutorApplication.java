b1583f81c3139f8948d7dd1c22228b0e85d4aac9
rtg <rtg1539@naver.com>
2024-06-19 15:15:56 +0900

    Chore: lombok 의존성, @EnableJpaAuditing 애너테이션 추가

--- src/main/java/kr/sesacjava/swimtutor/SwimmingTutorApplication.java
+++ src/main/java/kr/sesacjava/swimtutor/SwimmingTutorApplication.java
@@ -2,12 +2,14 @@ package kr.sesacjava.swimtutor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SwimmingTutorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwimmingTutorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwimmingTutorApplication.class, args);
+    }
 
 }
