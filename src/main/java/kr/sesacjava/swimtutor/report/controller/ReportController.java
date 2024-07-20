be1f631c8a7aadb6031ee3e5935d01851a0a39c9
rtg <rtg1539@naver.com>
2024-07-20 14:22:13 +0900

    Refactor: 레포트 API 수정

--- src/main/java/kr/sesacjava/swimtutor/report/controller/ReportController.java
+++ src/main/java/kr/sesacjava/swimtutor/report/controller/ReportController.java
@@ -1,12 +1,13 @@
 package kr.sesacjava.swimtutor.report.controller;
 
-import kr.sesacjava.swimtutor.records.entity.Record;
+import kr.sesacjava.swimtutor.record.entity.ExerciseRecord;
 import kr.sesacjava.swimtutor.report.service.ReportService;
+import kr.sesacjava.swimtutor.security.CurrentUser;
+import kr.sesacjava.swimtutor.security.dto.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,15 +19,10 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping
-    public ResponseEntity<List<Record>> getRecordsByOauthInfo(
-            @RequestParam("oauthLoginId") String oauthLoginId,
-            @RequestParam("oauthLoginPlatform") String oauthLoginPlatform
-    ) {
-        List<Record> records = reportService.getRecordsByOauthInfo(oauthLoginId, oauthLoginPlatform);
+    public ResponseEntity<List<ExerciseRecord>> getRecordsByOauthInfo(@CurrentUser UserInfo userInfo) {
+        List<ExerciseRecord> records = reportService.getRecordsByOauthInfo(userInfo);
         return ResponseEntity.ok(records);
     }
-
-
 
 
 }
--- src/main/java/kr/sesacjava/swimtutor/report/service/ReportService.java
+++ src/main/java/kr/sesacjava/swimtutor/report/service/ReportService.java
@@ -1,9 +1,10 @@
 package kr.sesacjava.swimtutor.report.service;
 
-import kr.sesacjava.swimtutor.records.entity.Record;
+import kr.sesacjava.swimtutor.record.entity.ExerciseRecord;
+import kr.sesacjava.swimtutor.security.dto.UserInfo;
 
 import java.util.List;
 
 public interface ReportService {
-    List<Record> getRecordsByOauthInfo(String oauthLoginId, String oauthLoginPlatform);
+    List<ExerciseRecord> getRecordsByOauthInfo(UserInfo userInfo);
 }
