e5491e422e2d5265258adfda9872d0aa21d07e7e
danmiee <dan-mi@kakao.com>
2024-07-28 20:52:12 +0900

    Feat: getCurrentLevel API 구현

--- src/main/java/kr/sesacjava/swimtutor/leveltest/repository/LevelRepository.java
+++ src/main/java/kr/sesacjava/swimtutor/leveltest/repository/LevelRepository.java
@@ -1,7 +1,17 @@
 package kr.sesacjava.swimtutor.leveltest.repository;
 
+import kr.sesacjava.swimtutor.leveltest.dto.LevelResponseDTO;
 import kr.sesacjava.swimtutor.leveltest.entity.Level;
+import kr.sesacjava.swimtutor.security.dto.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LevelRepository extends JpaRepository<Level, Long> {
+    // TODO: 현재 수영 레벨을 조회하는 API 구현
+    @Query("SELECT new kr.sesacjava.swimtutor.leveltest.dto.LevelResponseDTO(l.lcTrainingName, l.userLevel) " +
+            "FROM Level l WHERE l.oauthLoginId = :#{#userInfo.email} AND l.oauthLoginPlatform = :#{#userInfo.platform}")
+    List<LevelResponseDTO> findCurrentLevel(@Param("userInfo") UserInfo userInfo);
 }
